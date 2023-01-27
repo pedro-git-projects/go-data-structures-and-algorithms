498e441beb99d845be79d682ce5ed7aedf2a9197
pedro-git-projects <pedro.generic.email@gmail.com>
2023-01-27 16:17:02 -0300

    merge sort implementation missing side effects

--- sort/sort_test.go
+++ sort/sort_test.go
@@ -1,17 +1,15 @@
-package sort_test
+package sort
 
 import (
 	"reflect"
 	"testing"
-
-	"github.com/pedro-git-projects/go-data-structures-and-algorithms/sort"
 )
 
 func TestBubble(t *testing.T) {
 	got := []int{
 		4, 3, 2, 1, 5,
 	}
-	sort.Bubble(got)
+	Bubble(got)
 
 	expect := []int{
 		1, 2, 3, 4, 5,
@@ -27,7 +25,7 @@ func TestSelection(t *testing.T) {
 	got := []int{
 		4, 3, 2, 1, 5,
 	}
-	sort.Selection(got)
+	Selection(got)
 
 	expect := []int{
 		1, 2, 3, 4, 5,
@@ -42,7 +40,7 @@ func TestInsertion(t *testing.T) {
 	got := []int{
 		4, 3, 2, 1, 5,
 	}
-	sort.Insertion(got)
+	Insertion(got)
 
 	expect := []int{
 		1, 2, 3, 4, 5,
@@ -50,5 +48,21 @@ func TestInsertion(t *testing.T) {
 
 	if !reflect.DeepEqual(got, expect) {
 		t.Errorf("Expected %v, but got %v", expect, got)
+	}
+}
+
+func TestVMerge(t *testing.T) {
+	got := []int{
+		1, 3, 7, 8, 2, 4, 5, 6,
+	}
+
+	MergeSort(got)
+
+	want := []int{
+		1, 2, 3, 4, 5, 6, 7, 8,
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Expected %v, but got %v", want, got)
 	}
 }
